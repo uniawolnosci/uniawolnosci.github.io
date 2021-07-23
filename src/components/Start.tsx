5ee8428c998026c748fb27e8639e74c5d413c14c
Mateusz <mateuszslotwinski16@gmail.com>
2021-07-23 22:39:53 +0200

    gg

---
+++ src/components/Start.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from '../img/logo.svg';
+import '../css/main.css';
+
+function Start() {
+  return ( <div className="app">
+    <main className="index">
+      <header>
+        <h1>Unia Wolności</h1><h2>Zasługujemy na wolność!</h2>
+        <a className="indexmenu" href="/program">Zaczynajmy!</a>
+      </header>
+    </main>
+    </div>
+  );
+}
+
+export default Start;
